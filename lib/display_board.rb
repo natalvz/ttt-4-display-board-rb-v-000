bff43819fab975d47d9696dc5b1ae2db2976eb15
Natalia Castro <nat.alvz@gmail.com>
2018-04-25 04:39:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,10 +5,10 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
   cell = "   "
   separate = "|"
   row = "-----------"
-  puts "#{board[0]}#{separate}#{board[1]}#{separate}#{board[2]}"
-  puts "#{row}"
-  puts "#{board[3]}#{separate}#{board[4]}#{separate}#{board[5]}"
-  puts "#{row}"
-  puts "#{board[6]}#{separate}#{board[7]}#{separate}#{board[8]}"
+  puts "#{board[0]}#{separate}#{board[1]}#{separate}#{board[2]}";
+  puts "#{row}";
+  puts "#{board[3]}#{separate}#{board[4]}#{separate}#{board[5]}";
+  puts "#{row}";
+  puts "#{board[6]}#{separate}#{board[7]}#{separate}#{board[8]}";
 end
 
